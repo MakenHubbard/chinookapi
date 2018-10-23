1c6ff13ec3275ba637648d78699a1d5f016d4cc5
Maken Hubbard <makhub26@outlook.com>
2018-10-22 21:33:52 -0500

    adding some code wip

---
+++ chinookapi1/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chinookapi1
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int CustomerId { get; set; }
+        public int InvoiceDate { get; set; }
+        public string BillingAddress { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingPostalCode { get; set; }
+        public decimal Total { get; set; }
+        public string SalesRep { get; set; }
+    }
+}
